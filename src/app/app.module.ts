413a955c13f2796827d21d576c373defd9e1b5e9
donnasvinis <dvsvinis@mac.com>
2019-07-07 18:59:16 -0400

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { UserService } from './user.service';
+import { CoreModule } from './core/core.module';
+import { UserService } from './core/services/user.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     HomeComponent,
     ContactComponent
   ],
@@ -23,7 +21,8 @@ import { UserService } from './user.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [
     UserService
--- src/app/users/user-single/user-single.component.ts
+++ src/app/users/user-single/user-single.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user-single',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-single.component.scss']
 })
 export class UserSingleComponent implements OnInit {
+  user;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    //get the username from the url
+    this.route.params.subscribe(params => {
+      const username = params['username'];
+    // use the userService to get the data from github api
+    this.userService
+      .getUser(username)
+      .subscribe(user => this.user = user);
+    });
   }
 
 }
+
+
